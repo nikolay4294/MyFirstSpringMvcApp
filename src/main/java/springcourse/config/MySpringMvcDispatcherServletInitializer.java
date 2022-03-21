ebaf308f15e42f7366eae430239ae07851775f90
Nikolay4294 <boss1811@yandex.ru>
2022-03-21 08:41:28 +0300

    with web.xml and ApplicationContext.xml

---
+++ src/main/java/springcourse/config/MySpringMvcDispatcherServletInitializer.java
@@ -0,0 +1,21 @@
+package springcourse.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import springcourse.SpringConfig;
+
+public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override // заменяет web.xml
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
