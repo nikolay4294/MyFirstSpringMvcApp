08459ffcd3ed55691c2a15fb586711d537a46f70
Nikolay4294 <boss1811@yandex.ru>
2022-04-02 19:19:31 +0300

    02/04/22 @RequestParam

--- src/main/java/springcourse/controllers/FirstController.java
+++ src/main/java/springcourse/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package springcourse.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,11 +21,43 @@ public class FirstController {
         return "first/hello";
     }
 
-    @GetMapping("goodbye")
-    public String goodByePage(@RequestParam(value = "name", required = false) String name,
-                              @RequestParam(value = "surname", required = false) String surname) { // c помощью спринга
+    @GetMapping("/goodbye")
+    public String goodByePage(@RequestParam(value = "name", required = false) String name,   // c помощью спринга
+                              @RequestParam(value = "surname", required = false) String surname,
+                              Model model) {
 
-        System.out.println("GoodBye " + name + " " + surname);
+        //System.out.println("GoodBye " + name + " " + surname);
+
+        model.addAttribute("message", "GoodBye " + name + " " + surname);
         return "first/goodbye";
     }
+
+    @GetMapping("/calculator")
+    public String calculatorPage(@RequestParam(value = "a", required = false) int a,
+                                 @RequestParam(value = "b", required = false) int b,
+                                 @RequestParam(value = "action", required = false) String action,
+                                 Model model) {
+        model.addAttribute("action", getResult(a, b, action));
+        return "/first/calculator";
+    }
+
+    public double getResult(int a, int b, String action) {
+        double result = 0.0;
+        switch (action) {
+            case "multiplication":
+                result = a * b;
+                break;
+            case "addition":
+                result = a + b;
+                break;
+            case "subtraction":
+                result = a - b;
+                break;
+            case "division":
+                result = a / (double)b;
+                break;
+        }
+        return result;
+    }
 }
+
